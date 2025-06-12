80405444cfe3ff38a597d6c243b8ac29b5e0e9d4
juanjpl <juan_jpl@hotmail.com>
2025-06-12 16:07:58 -0300

    commit 001

--- index.js
+++ index.js
@@ -2,9 +2,18 @@
 
 import express from "express";
 import usuarioRoutes from "./routes/usuarioRoutes.js";
+import db from "./config/db.js";
 
 //crear la app
 const app = express();
+
+//Conexion a la base de datos
+try {
+    await db.authenticate();
+    console.log("Conexion correcta a la base de datos!")
+} catch (error) {
+    console.log(error)
+}
 
 //Habilitar Pug
 app.set("view engine", "pug");
